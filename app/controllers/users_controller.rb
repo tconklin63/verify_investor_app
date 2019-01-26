cf236cec4800155cdeb1a77f64418ea4b8650907
Thomas Conklin <tconklin63@gmail.com>
2019-01-26 13:40:47 -0800

    Add file uploads and downloads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def show
+    @files = UploadedFile.where(user_id: current_user.id).order(filename: :asc)
+  end
+
+  def upload_file
+    if params[:file]
+      UploadedFile.create(user_id: current_user.id,
+                          filename: params[:file].original_filename,
+                          data: params[:file].read,
+                          extension: params[:file].original_filename.split('.').last)
+    else
+      flash[:warning] = "No file selected"
+    end
+    redirect_to current_user
+  end
+
+  def download_file
+    file = UploadedFile.find(params[:file_id])
+    send_data file.data, filename: file.filename, disposition: 'inline'
+  end
+
+  def delete_file
+    UploadedFile.find(params[:file_id]).delete
+    redirect_to current_user
   end
 end
